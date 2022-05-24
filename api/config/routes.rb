47a2ae8016410d07d81dff4b3ff51ad6be0542a8
Jason Cruz <jason1642@gmail.com>
2022-05-24 00:38:03 -0400

    routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   # get 'user/find-all', to: 'users#index'
   # get 'user/:id/find-one', to: 'users#findOneById'
 
+  # To make sure a paramter matches a regular expression
+  # resources :users, constraints: { id: /[A-Z]/}
+  # Look at member and collection block to add unique restful actions 
 
   resources :users
-
 
   # Use this when creating the posts model/controllers
   # resources :users do
