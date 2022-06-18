b88f2afe22452a19c5c31bebb7b573cb2b5e43e5
Jason Cruz <jason1642@gmail.com>
2022-06-17 22:15:03 -0400

    messenger

--- components/comment/Card.tsx
+++ components/comment/Card.tsx
@@ -37,7 +37,7 @@ const Card: React.FunctionComponent<ICardProps> = ({colors, commentData, navigat
 };
 
 export default Card;
-
+ 
 
 const useStyles = makeStyles((theme, props: any) => ({
   container: {
